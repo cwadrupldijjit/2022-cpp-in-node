7490b054eaeec642434dde6ae3d7a3b3122bdea6
Samuel Skeen <sskeen9@gmail.com>
2022-09-23 12:07:20 -0600

    tweak example slightly to use createRequire instead of dlopen directly
    also did a little cleanup and clarification in the configs

--- cpp/index.js
+++ cpp/index.js
@@ -4,8 +4,10 @@ import { createRequire } from 'module';
 // Since __dirname doesn't exist in esm:
 const __dirname = new URL(dirname(import.meta.url)).pathname.slice(process.platform == 'win32' ? 1 : 0);
 
-// node addons currently only support being "require"d in CommonJS, so we have to create our own "require" function to grab it
-// the require function will run the dlopen on the node addon so you don't have to
+// node addons currently only support being "require"d in CommonJS, so since ESM doesn't have a
+// "require" function, we have to create our own "require" function in order to open it.
+// the require function will run the node internal "dlopen" function on the node addon so you
+// don't have to do it manually
 const require = createRequire(import.meta.url);
 
 export default require(
